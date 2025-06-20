84e86d4bb3a5232806dfb9f89846e708b4cbe20f
Manuel Ponsa <manuponsa@gmail.com>
2025-06-20 11:53:47 -0300

    feat: add new fields to tap

--- tap_opensea/streams.py
+++ tap_opensea/streams.py
@@ -30,26 +30,37 @@ class OrdersStream(OpenseaStream):
         """Convert parcels into psv and adds block number"""
         newrow = {}
 
-        # Core
+        # Core fields
         newrow['transaction_hash'] = str(row['transaction'])
+        newrow['event_type'] = row.get('event_type')
+        newrow['order_hash'] = row.get('order_hash')
+        newrow['chain'] = row['chain']
+        newrow['protocol_address'] = row['protocol_address']
+        newrow['closing_date'] = row.get('closing_date')
+        
         if 'quantity' in row:
             if row.get('quantity') is not None:
                 newrow['quantity'] = int(row.get('quantity'))
-        newrow['chain'] = row['chain']
 
-        # NFT
+        # NFT fields
         nft = row.get('nft')
-
         if nft is not None:
             newrow['nft_address'] = nft.get('contract')
             newrow['nft_collection_name'] = nft.get('collection')
             newrow['nft_identifier'] = nft.get('identifier')
+            newrow['nft_token_standard'] = nft.get('token_standard')
             newrow['nft_name'] = nft.get('name')
             newrow['nft_description'] = nft.get('description')
+            newrow['nft_image_url'] = nft.get('image_url')
+            newrow['nft_display_image_url'] = nft.get('display_image_url')
+            newrow['nft_display_animation_url'] = nft.get('display_animation_url')
             newrow['nft_metadata_url'] = nft.get('metadata_url')
             newrow['nft_opensea_url'] = nft.get('opensea_url')
+            newrow['nft_updated_at'] = nft.get('updated_at')
+            newrow['nft_is_disabled'] = nft.get('is_disabled')
+            newrow['nft_is_nsfw'] = nft.get('is_nsfw')
 
-        # Payment
+        # Payment fields
         payment = row.get('payment')
         if payment:
             newrow['payment_symbol'] = payment.get('symbol')
@@ -57,25 +68,37 @@ class OrdersStream(OpenseaStream):
             newrow['payment_amount'] = str(payment.get('quantity'))
             newrow['decimals'] = str(payment.get('decimals'))
 
-        # Seller
+        # Seller and buyer
         newrow['seller_address'] = row.get('seller')
         newrow['buyer_address'] = row.get('buyer')
 
+        # Timestamp
         newrow['timestamp'] = str(row.get('event_timestamp'))
 
         return newrow
 
     schema = th.PropertiesList(
         th.Property("transaction_hash", th.StringType),
-        th.Property("quantity", th.IntegerType),
+        th.Property("event_type", th.StringType),
+        th.Property("order_hash", th.StringType),
         th.Property("chain", th.StringType),
+        th.Property("protocol_address", th.StringType),
+        th.Property("closing_date", th.IntegerType),
+        th.Property("quantity", th.IntegerType),
         th.Property("nft_address", th.StringType),
         th.Property("nft_collection_name", th.StringType),
         th.Property("nft_identifier", th.StringType),
+        th.Property("nft_token_standard", th.StringType),
         th.Property("nft_name", th.StringType),
         th.Property("nft_description", th.StringType),
+        th.Property("nft_image_url", th.StringType),
+        th.Property("nft_display_image_url", th.StringType),
+        th.Property("nft_display_animation_url", th.StringType),
         th.Property("nft_metadata_url", th.StringType),
         th.Property("nft_opensea_url", th.StringType),
+        th.Property("nft_updated_at", th.StringType),
+        th.Property("nft_is_disabled", th.BooleanType),
+        th.Property("nft_is_nsfw", th.BooleanType),
         th.Property("payment_symbol", th.StringType),
         th.Property("payment_token_address", th.StringType),
         th.Property("payment_amount", th.StringType),
